b8d8beb87c2952bdf1151f46223015788815b6b9
katafuchix <katafuchix@gmail.com>
2026-01-30 17:52:53 +0900

    Initial Commit

---
+++ SwiftUI_MapCarouselExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_MapCarouselExample
+//
+//  Created by cano on 2026/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_MapCarouselExample/SwiftUI_MapCarouselExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapCarouselExampleApp.swift
+//  SwiftUI_MapCarouselExample
+//
+//  Created by cano on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapCarouselExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
